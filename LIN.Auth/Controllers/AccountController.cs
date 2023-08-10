756f268eb5d048751f6dd0d9fb4123612a5dc9fc
Alex <giraldojhong4@gmail.com>
2023-08-10 18:09:23 -0500

    Separacion de rutas

--- LIN.Auth/Controllers/AccountController.cs
+++ LIN.Auth/Controllers/AccountController.cs
@@ -97,87 +97,6 @@ public class AccountController : ControllerBase
 
 
     /// <summary>
-    /// Inicia una sesión de usuario
-    /// </summary>
-    /// <param name="user">Usuario único</param>
-    /// <param name="password">Contraseña del usuario</param>
-    [HttpGet("login")]
-    public async Task<HttpReadOneResponse<AccountModel>> Login([FromQuery] string user, [FromQuery] string password)
-    {
-
-        // Comprobación
-        if (!user.Any() || !password.Any())
-            return new(Responses.InvalidParam);
-
-
-        // Obtiene el usuario
-        var response = await Data.Users.Read(user, true);
-
-        if (response.Response != Responses.Success)
-            return new(response.Response);
-
-        if (response.Model.Estado != AccountStatus.Normal)
-            return new(Responses.NotExistAccount);
-
-        if (response.Model.Contraseña != EncryptClass.Encrypt(Conexión.SecreteWord + password))
-            return new(Responses.InvalidPassword);
-
-        // Genera el token
-        var token = Jwt.Generate(response.Model);
-
-        // Crea registro del login
-        _ = Data.Logins.Create(new()
-        {
-            Date = DateTime.Now,
-            AccountID = response.Model.ID
-        });
-
-        response.Token = token;
-        return response;
-
-    }
-
-
-
-    /// <summary>
-    /// Inicia una sesión de usuario por medio del token
-    /// </summary>
-    /// <param name="token">Token de acceso</param>
-    [HttpGet("LoginWithToken")]
-    public async Task<HttpReadOneResponse<AccountModel>> LoginWithToken([FromHeader] string token)
-    {
-
-        // Valida el token
-        (var isValid, var user, var _) = Jwt.Validate(token);
-
-        if (!isValid)
-            return new(Responses.InvalidParam);
-
-
-        // Obtiene el usuario
-        var response = await Data.Users.Read(user, true);
-
-        if (response.Response != Responses.Success)
-            return new(response.Response);
-
-        if (response.Model.Estado != AccountStatus.Normal)
-            return new(Responses.NotExistAccount);
-
-        // Crea registro del login
-        _ = Data.Logins.Create(new()
-        {
-            Date = DateTime.Now,
-            AccountID = response.Model.ID
-        });
-
-        response.Token = token;
-        return response;
-
-    }
-
-
-
-    /// <summary>
     /// Obtiene un usuario por medio de el usuario Unico
     /// </summary>
     /// <param name="user">Usuario</param>
@@ -225,11 +144,6 @@ public class AccountController : ControllerBase
 
         return response;
     }
-
-
-
-
-
 
 
 
@@ -321,91 +235,6 @@ public class AccountController : ControllerBase
 
 
     /// <summary>
-    /// Actualiza la contraseña
-    /// </summary>
-    /// <param name="modelo">Nuevo modelo</param>
-    [HttpPatch("update/password")]
-    public async Task<HttpResponseBase> Update([FromBody] UpdatePasswordModel modelo)
-    {
-
-        if (modelo.Account <= 0 || modelo.OldPassword.Length < 4 || modelo.NewPassword.Length < 4)
-            return new(Responses.InvalidParam);
-
-
-        var actualData = await Data.Users.Read(modelo.Account, true);
-
-        if (actualData.Response != Responses.Success)
-            return new(Responses.NotExistAccount);
-
-        var oldEncrypted = actualData.Model.Contraseña;
-
-
-        if (oldEncrypted != actualData.Model.Contraseña)
-        {
-            return new ResponseBase(Responses.InvalidPassword);
-        }
-
-        return await Data.Users.UpdatePassword(modelo);
-
-    }
-
-
-
-    /// <summary>
-    /// Elimina una cuenta
-    /// </summary>
-    /// <param name="id">ID del usuario</param>
-    [HttpDelete("delete")]
-    public async Task<HttpResponseBase> Delete([FromHeader] string token)
-    {
-
-        var (isValid, _, userID) = Jwt.Validate(token);
-
-        if (!isValid)
-            return new ResponseBase
-            {
-                Response = Responses.Unauthorized,
-                Message = "Token invalido"
-            };
-
-        if (userID <= 0)
-            return new(Responses.InvalidParam);
-
-        var response = await Data.Users.Delete(userID);
-        return response;
-    }
-
-
-
-    /// <summary>
-    /// Actualiza los datos de un usuario
-    /// </summary>
-    /// <param name="modelo">Nuevo modelo</param>
-    [HttpPatch("disable/account")]
-    public async Task<HttpResponseBase> Disable([FromBody] AccountModel user)
-    {
-
-        if (user.ID <= 0)
-        {
-            return new(Responses.ExistAccount);
-        }
-
-        // Modelo de usuario de la BD
-        var userModel = await Data.Users.Read(user.ID, false);
-
-        if (userModel.Model.Contraseña != EncryptClass.Encrypt(Conexión.SecreteWord + user.Contraseña))
-        {
-            return new(Responses.InvalidPassword);
-        }
-
-
-        return await Data.Users.UpdateState(user.ID, AccountStatus.Disable);
-
-    }
-
-
-
-    /// <summary>
     /// Actualiza el genero de un usuario
     /// </summary>
     /// <param name="token">Token de acceso</param>
@@ -424,27 +253,6 @@ public class AccountController : ControllerBase
         }
 
         return await Data.Users.UpdateGender(id, genero);
-
-    }
-
-
-
-    /// <summary>
-    /// Obtiene los mails asociados a una cuenta
-    /// </summary>
-    /// <param name="token">Token de acceso</param>
-    [HttpGet("mails")]
-    public async Task<HttpReadAllResponse<EmailModel>> GetMails([FromHeader] string token)
-    {
-
-        var (isValid, _, id) = Jwt.Validate(token);
-
-        if (!isValid)
-        {
-            return new(Responses.Unauthorized);
-        }
-
-        return await Data.Mails.ReadAll(id);
 
     }
 
---
+++ LIN.Auth/Controllers/MailController.cs
@@ -0,0 +1,30 @@
+namespace LIN.Auth.Controllers;
+
+
+[Route("mails")]
+public class MailController : ControllerBase
+{
+
+
+    /// <summary>
+    /// Obtiene los mails asociados a una cuenta
+    /// </summary>
+    /// <param name="token">Token de acceso</param>
+    [HttpGet("all")]
+    public async Task<HttpReadAllResponse<EmailModel>> GetMails([FromHeader] string token)
+    {
+
+        var (isValid, _, id) = Jwt.Validate(token);
+
+        if (!isValid)
+        {
+            return new(Responses.Unauthorized);
+        }
+
+        return await Data.Mails.ReadAll(id);
+
+    }
+
+
+
+}
