50aa943c95d3aac832a2545a91e17700f90a1a59
Alex <giraldojhong4@gmail.com>
2023-08-10 14:46:58 -0500

    Status Route

---
+++ LIN.Auth/Controllers/ApiController.cs
@@ -0,0 +1,32 @@
+namespace LIN.Auth.Controllers;
+
+
+[Route("/")]
+public class APIVersion : Controller
+{
+
+
+
+    /// <summary>
+    /// Obtiene el estado del servidor
+    /// </summary>
+    [HttpGet("status")]
+    public dynamic Status()
+    {
+        return StatusCode(200, new
+        {
+            Status = "Open"
+        });
+    }
+
+
+
+
+    [HttpGet]
+    public dynamic Index()
+    {
+        return Ok("Abierto");
+    }
+
+
+}
