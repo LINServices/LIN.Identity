0405f1703f7cf31232697ae8525a2fa7457dd5f8
Alex <giraldojhong4@gmail.com>
2023-08-15 15:16:48 -0500

    Nuevo controlador para dispositivos

---
+++ LIN.Auth/Controllers/DeviceController.cs
@@ -0,0 +1,51 @@
+namespace LIN.Auth.Controllers;
+
+
+[Route("devices")]
+public class DeviceController : ControllerBase
+{
+
+
+    /// <summary>
+    /// Obtiene la lista de dispositivos asociados a una cuenta en tiempo real
+    /// </summary>
+    /// <param name="token">Token de acceso</param>
+    [HttpGet]
+    public HttpReadAllResponse<DeviceModel> GetAll([FromHeader] string token)
+    {
+        try
+        {
+
+            var (isValid, _, userID) = Jwt.Validate(token);
+
+            if (!isValid)
+            {
+                return new ReadAllResponse<DeviceModel>
+                {
+                    Message = "Token Invalido",
+                    Response = Responses.Unauthorized
+                };
+            }
+
+
+            var devices = (from A in Hubs.AccountHub.Cuentas
+                           where A.Key == userID
+                           select A).FirstOrDefault().Value ?? new();
+
+
+            var filter = (from D in devices
+                          where D.Estado == DeviceState.Actived
+                          select D).ToList();
+
+            // Retorna
+            return new(Responses.Success, filter ?? new());
+        }
+        catch
+        {
+            return new(Responses.Undefined);
+        }
+    }
+
+
+
+}
