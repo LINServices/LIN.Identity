07d89f3820b0dd0c207014a695a3209f435d6b0a
Alex <giraldojhong4@gmail.com>
2023-12-09 10:26:31 -0500

    Politicas DATA

---
+++ LIN.Identity/Data/Policies.cs
@@ -0,0 +1,114 @@
+﻿namespace LIN.Identity.Data;
+
+
+public class Policies
+{
+
+
+    #region Abstracciones
+
+
+  
+    /// <summary>
+    /// Crear nueva política.
+    /// </summary>
+    /// <param name="data">Modelo.</param>
+    public static async Task<CreateResponse> Create(PolicyModel data)
+    {
+        var (context, contextKey) = Conexión.GetOneConnection();
+        var response = await Create(data, context);
+        context.CloseActions(contextKey);
+        return response;
+    }
+
+
+
+    /// <summary>
+    /// Obtener las políticas.
+    /// </summary>
+    /// <param name="id">Id de la cuenta.</param>
+    public static async Task<ReadAllResponse<PolicyModel>> ReadAll(int id)
+    {
+        var (context, contextKey) = Conexión.GetOneConnection();
+
+        var res = await ReadAll(id, context);
+        context.CloseActions(contextKey);
+        return res;
+    }
+
+
+
+    #endregion
+
+
+
+    /// <summary>
+    /// Crear política.
+    /// </summary>
+    /// <param name="data">Modelo</param>
+    /// <param name="context">Contexto de conexión.</param>
+    public static async Task<CreateResponse> Create(PolicyModel data, Conexión context)
+    {
+
+        // ID.
+        data.Id = 0;
+
+        // Ejecución
+        try
+        {
+
+            // Existe el directorio.
+            context.DataBase.Attach(data.Directory);
+
+            // Guardar la información.
+            await context.DataBase.Policies.AddAsync(data);
+
+            // Llevar info a la BD.
+            context.DataBase.SaveChanges();
+
+            return new(Responses.Success, data.Id);
+        }
+        catch (Exception ex)
+        {
+
+            if ((ex.InnerException?.Message.Contains("Violation of UNIQUE KEY constraint") ?? false) || (ex.InnerException?.Message.Contains("duplicate key") ?? false))
+                return new(Responses.Undefined);
+
+        }
+
+        return new();
+    }
+
+
+
+    /// <summary>
+    /// Obtiene las políticas asociadas a un usuario.
+    /// </summary>
+    /// <param name="id">ID de la cuenta</param>
+    /// <param name="context">Contexto de conexión</param>
+    public static async Task<ReadAllResponse<PolicyModel>> ReadAll(int id, Conexión context)
+    {
+
+        // Ejecución
+        try
+        {
+
+            var policies = await (from directory in context.DataBase.DirectoryMembers
+                                  where directory.AccountId == id
+                                  join policie in context.DataBase.Policies
+                                  on directory.DirectoryId equals policie.DirectoryId
+                                  select policie).ToListAsync();
+
+
+            return new(Responses.Success, policies);
+
+        }
+        catch
+        {
+        }
+
+        return new();
+    }
+
+
+}
