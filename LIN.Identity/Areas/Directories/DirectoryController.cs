39bfcc003cc81819193bd7ff3d4b58531dd5c39c
Alex <giraldojhong4@gmail.com>
2023-12-08 22:09:09 -0500

    Mejoras con Directory

--- LIN.Identity/Areas/Directories/DirectoryController.cs
+++ LIN.Identity/Areas/Directories/DirectoryController.cs
@@ -45,4 +45,41 @@ public class DirectoryController : ControllerBase
     }
 
 
+
+    /// <summary>
+    /// Obtener los directorios asociados.
+    /// </summary>
+    /// <param name="token">Token de acceso.</param>
+    [HttpGet("read/all")]
+    public async Task<HttpReadAllResponse<DirectoryMember>> ReadAll([FromHeader] string token)
+    {
+
+        // Información del token.
+        var (isValid, _, user, _, _) = Jwt.Validate(token);
+
+        // Token es invalido.
+        if (!isValid)
+            return new ReadAllResponse<DirectoryMember>()
+            {
+                Response = Responses.Unauthorized,
+                Message = "Token invalido."
+            };
+
+        // Obtiene el usuario.
+        var response = await Data.DirectoryMembers.ReadAll(user);
+
+        // Si es erróneo
+        if (response.Response != Responses.Success)
+            return new()
+            {
+                Response = response.Response
+            };
+
+        // Retorna el resultado
+        return response;
+
+    }
+
+
+
 }
