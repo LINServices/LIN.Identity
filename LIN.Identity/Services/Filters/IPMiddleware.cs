9b78204158569c9f054215e46eb425d4a18939af
Alex <giraldojhong4@gmail.com>
2023-12-27 14:03:35 -0500

    Mejoras en los middlewares

---
+++ LIN.Identity/Services/Filters/IPMiddleware.cs
@@ -0,0 +1,46 @@
+﻿namespace LIN.Identity.Services.Filters;
+
+
+public class IPMiddleware : IMiddleware
+{
+
+    /// <summary>
+    /// Middleware de IP.
+    /// </summary>
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        // Obtener la IP.
+        var ip = context.Connection.RemoteIpAddress;
+
+        // Validar la IP.
+        if (ip == null)
+        {
+            context.Response.StatusCode = 400;
+            await context.Response.WriteAsJsonAsync(new ResponseBase
+            {
+                Message = "La IP del cliente no pudo ser resuelta.",
+                Response = Responses.Unauthorized
+            });
+            return;
+        }
+
+        // Item de IP.
+        context.Items.Add("IP", ip);
+        await next(context);
+    }
+
+}
+
+
+public static class IPMiddlewareExtensions
+{
+    public static IApplicationBuilder UseIP(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<IPMiddleware>();
+    }
+
+    public static IServiceCollection AddIP(this IServiceCollection builder)
+    {
+        return builder.AddSingleton<IPMiddleware>();
+    }
+}
