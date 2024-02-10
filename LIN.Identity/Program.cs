cfa889b34c5fbba8f3d077b81a6a860bb201201a
Alex <giraldojhong4@gmail.com>
2024-02-09 19:00:51 -0500

    -> LIN.Cloud.Identity

--- LIN.Identity/Program.cs
+++ LIN.Identity/Program.cs
@@ -40,12 +40,10 @@ try
 
 
     builder.Services.AddControllers();
-    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
     builder.Services.AddEndpointsApiExplorer();
     builder.Services.AddSwaggerGen();
 
     var app = builder.Build();
-
 
     try
     {
