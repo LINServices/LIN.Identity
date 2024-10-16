b196ff68e7dfd8dbb56961bdb39d42b274cce3a0
Alexander Giraldo <giraldojhong4@gmail.com>
2024-10-15 22:31:39 -0500

    Update

--- LIN.Cloud.Identity/Program.cs
+++ LIN.Cloud.Identity/Program.cs
@@ -2,6 +2,7 @@ using Http.Extensions;
 using LIN.Cloud.Identity.Services.Auth.Interfaces;
 using LIN.Cloud.Identity.Services.Extensions;
 using LIN.Cloud.Identity.Services.Realtime;
+using Microsoft.AspNetCore.Http.HttpResults;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,11 +15,14 @@ builder.Services.AddLINHttp(true, (options) =>
 
 builder.Services.AddLocalServices();
 
+
 // Servicio de autenticación.
 builder.Services.AddScoped<IAuthentication, Authentication>();
 builder.Services.AddPersistence(builder.Configuration);
 
 var app = builder.Build();
+
+
 
 app.UseLINHttp();
 
@@ -30,5 +34,12 @@ app.MapHub<PassKeyHub>("/realTime/auth/passkey");
 
 app.UseAuthorization();
 app.MapControllers();
+
+builder.Services.AddDatabaseAction(() =>
+{
+    var context = app.Services.GetRequiredService<DataContext>();
+    context.Accounts.Where(x => x.Id == 0).FirstOrDefaultAsync();
+    return "Success";
+});
 
 app.Run();
